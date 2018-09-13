160bcc32fa6e1e36120ff34f60714081bedabbed
Bruce <brucewmacdonald@gmail.com>
2018-09-12 21:07:02 -0700

    add new icons and broaden host check

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Jira-Description-Templates
 Autofill Jira ticket descriptions from a template
+
+## Future Work
+It would be nice to have the ability to inject buttons corresponding to templates into the actual page, but that functionality is currently difficult in single page web applications without doing something inefficient.
+
+## Assets Used
+Application Icon - BomSymbols - https://creativemarket.com/BomSymbols
--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const host = 'atlassian.net';
+
 chrome.runtime.onInstalled.addListener(function() {
   // Start by loading any locally stored templates from previous sessions
   chrome.storage.sync.get(["storedTemplates"], function(templates) {
@@ -7,12 +9,12 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log('Stored tempates:');
     console.log(templates);
   });
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         // Enable the extension when on an Atlassian or Jira domain
-        // TODO: make more broad
-        pageUrl: {hostEquals: 'brucetesting.atlassian.net'},
+        pageUrl: {urlMatches: host},
         css: ["textarea[id='description']"]
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
